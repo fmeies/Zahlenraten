0bb5803195f112ed2368a4102773c62844d03270
Frank Meies <frank@macernie.fritz.box>
2015-01-25 13:39:15 +0100

    Initial commit

---
+++ Zahlenraten/Game.swift
@@ -0,0 +1,41 @@
+//
+//  Game.swift
+//  Zahlenraten
+//
+//  Created by Frank Meies on 25.01.15.
+//  Copyright (c) 2015 Frank Meies. All rights reserved.
+//
+
+import Foundation
+
+class Game{
+    
+    var status : UInt = 0
+    var moves: UInt = 0
+    var untergrenze : UInt = 0
+    var obergrenze : UInt = 0
+    var result : UInt = 0
+    
+    func reset(untergrenze: UInt, obergrenze: UInt){
+        status = 0
+        moves = 0
+        self.untergrenze = untergrenze
+        self.obergrenze = obergrenze
+        result = UInt(arc4random_uniform(obergrenze - untergrenze + 1))
+    }
+    
+    func guess(value: UInt){
+        if(value < result){
+            status = 1
+            ++moves
+        } else if(value > result){
+            status = 2
+            ++moves
+        } else if (value == result){
+            status = 3
+            ++moves
+        } else if (value < untergrenze || value > obergrenze){
+            status = 4
+        }
+    }
+}
--- Zahlenraten/ViewController.swift
+++ Zahlenraten/ViewController.swift
@@ -10,6 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var introLabel: UILabel!
+    @IBOutlet weak var inputField: UITextField!
+    @IBOutlet weak var lowerBoundaryField: UITextField!
+    @IBOutlet weak var upperBoundaryField: UITextField!
+    @IBOutlet weak var inputButton: UIButton!
+	@IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var movesLabel: UILabel!
+
+	var game:Game
+	
+	required init(coder aDecoder: NSCoder)
+	{
+		self.game = Game()
+		super.init(coder: aDecoder)
+	}
+	
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +35,90 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+	
+	@IBAction func newGamePressed() {
+		newGame()
+  	}
+    
+	@IBAction func buttonPressed() {
+        if(inputField.hidden){
+            startGame()
+        } else {
+            nextGuess()
+        }
+    }
+	
+    func nextGuess(){
+		var inputInt : UInt = 0
+		if let input = inputField.text?.toInt() {
+			inputInt = UInt(input)
+		}
+		
+        game.guess(inputInt)
+        updateStatus()
+    }
+    
+    func newGame(){
+		switchMode(false)
+		movesLabel.text = "Wähle die Grenzen: "
+		messageLabel.text = "Warten auf Eingabe!"
+    }
+	
+    func startGame(){
+		var lowerInt : UInt = 0
+		var upperInt : UInt = 0
+		
+		if let lowerInput = lowerBoundaryField.text?.toInt() {
+			lowerInt = UInt(lowerInput)
+		} else {
+			lowerBoundaryField.text = "1"
+			return
+		}
+		
+		if let upperInput = upperBoundaryField.text?.toInt() {
+			upperInt = UInt(upperInput)
+		} else {
+			upperBoundaryField.text = "100"
+			return
+		}
 
+		switchMode(true)
+		game.reset(lowerInt, obergrenze: upperInt)
+		updateStatus()
+    }
+	
+	func switchMode(startGame : Bool){
+		lowerBoundaryField.hidden = startGame
+		upperBoundaryField.hidden = startGame
+		inputField.hidden = !startGame
+		inputField.enabled = startGame
+		inputButton.enabled = true
+	}
+	
+	func updateStatus() {
+		inputField.text = ""
 
+		var moves = "Jetzt kommt dein " + String(game.moves + 1) + ". Zug"
+		var message = "Bitte die Grenzen beachten!"
+
+		var status : UInt = game.status;
+		if(status == 0){
+			message = "Gib eine Zahl ein!"
+		}
+		if(status == 1){
+			message = "Die Zahl ist zu klein!"
+		} else if (status == 2){
+			message = "Die Zahl ist zu gross!"
+		} else if (status == 3){
+			moves = "Juhu, du hast gewonnen!!!"
+			message =  "Du hast " + String(game.moves) + " Züge gebraucht."
+			inputField.enabled = false
+			inputButton.enabled = false
+		}
+		
+		messageLabel.text = message
+		movesLabel.text = moves
+
+	}
 }
 
